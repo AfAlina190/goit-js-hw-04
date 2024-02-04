e96d366dab1f64837308350061c5b6b9be36f2bd
ALINA <130515756+AfAlina190@users.noreply.github.com>
2024-02-04 20:23:10 +0100

    Home work modul 4

---
+++ js/task-1.js
@@ -0,0 +1,27 @@
+// task-1.js
+
+function isEnoughCapacity(products, containerSize) {
+    
+    let totalQuantity = Object.values(products).reduce((acc, quantity) => acc + quantity, 0);
+  
+    
+    return totalQuantity <= containerSize;
+  }
+  
+  // Перевірка функції
+  console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
+  
